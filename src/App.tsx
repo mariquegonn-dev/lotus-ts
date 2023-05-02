b1083dcec34dae5e84c9eae5db9678d60675efaa
Rique Gonn <ocatodev@gmail.com>
2023-05-02 18:37:54 -0300

    feat: caloriasDiarias route created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
+import CaloriasDiarias from "./Components/Rotas";
 
 const App = () => {
   return (
-    <section>
+    <section className="container">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="caloriasDiarias" element={<CaloriasDiarias />} />
         </Routes>
       </BrowserRouter>
     </section>
